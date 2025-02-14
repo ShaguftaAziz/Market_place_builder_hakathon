4829b4e7614567ebf4ca1746d5a7ab072f52f714
ShaguftaAziz <165736373+ShaguftaAziz@users.noreply.github.com>
2025-02-14 19:29:43 +0500

    Update HomeMenu.tsx

--- src/components/HomePages/HomeMenu.tsx
+++ src/components/HomePages/HomeMenu.tsx
@@ -1,4 +1,4 @@
-// src/componets/HomePages/HomeMenu.tsx
+src/componets/HomePages/HomeMenu.tsx
 import React from 'react';
 import { Great_Vibes } from 'next/font/google';
 import Image from 'next/image';
